a8075e9b2ae735538bb70b61087e85154c5419e8
Xiaolin Wang <exgphe@icloud.com>
2020-08-13 16:15:07 +0800

    Initial Commit

---
+++ Solar_Conqueror_Sources/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Solar_Conqueror_Sources
+//
+//  Created by 왕소림 on 2020/8/13.
+//  Copyright © 2020 Xiaolin Wang. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
